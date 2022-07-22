6b57853841575dff97a7cafed7c42a79401d6a94
satyal <satyalsubin33@gmail.com>
2022-07-22 10:45:04 +0545

    adding new urls in todo app

--- TODO/blog_app/urls.py
+++ TODO/blog_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("home/",views.home,name="home"),
-    path("add/",views.add,name="add")
+    path("add/",views.add,name="add"),
+    path("delete/<int:id>/",views.delete,name="delete"),
+    path("edit/<int:id>/",views.edit,name="edit"),
+    path("delete-all/",views.delete_all,name="delete-all"),
 ]
