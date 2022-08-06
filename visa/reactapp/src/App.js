3b4caf5870634c4d2ca70ff81361776e8c26dc09
satyal <satyalsubin33@gmail.com>
2022-08-06 21:25:31 +0545

    all the files and folder of react app

---
+++ visa/reactapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Formss from "./components/form";
+
+function App() {
+  return (
+    <div className="App">
+      <Formss />
+    </div>
+  );
+}
+
+export default App;
