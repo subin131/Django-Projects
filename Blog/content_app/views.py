f4ce0117590e71af012cc66135213e3d0157abcb
satyal <satyalsubin33@gmail.com>
2022-08-08 23:38:53 +0545

    adding the add and edit function

--- Blog/content_app/views.py
+++ Blog/content_app/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from multiprocessing import AuthenticationError
+from django.shortcuts import render,redirect
+from content_app.models import Blog
 
 # Create your views here
 def home(request):
-    return render(request,"content_app/home.html")
+    blogs=Blog.objects.all()
+    return render(request,"contentApp/home.html", context={'blogs':blogs})
+
+def add(request):
+    if request.method=="GET":
+        return render(request,"contentApp/add.html")
+    else:
+        t=request.POST["title"]
+        c=request.POST["content"]
+        Blog.objects.create(title=t,content=c,authors_id=1)
+        return redirect("home")
+    
+def edit(request,id):
+    blogs=Blog.objects.get(id=id)
+    if request.method=="GET":
+        return render (request, "contentApp/edit.html",{"blogs":blogs})
+    else:
+        blog.title=request.POST["title"]
+        blog.content=request.POST["content"]
+        blog.save()
+        return redirect("home")
