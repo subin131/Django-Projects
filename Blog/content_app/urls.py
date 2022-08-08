afdb0ef25fdaeb621201c935462388f8558a0359
satyal <satyalsubin33@gmail.com>
2022-08-08 23:38:10 +0545

    adding the urls like add and edit

--- Blog/content_app/urls.py
+++ Blog/content_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from content_app import views
 urlpatterns = [
-    path("home/",views.home)
+    path("home/",views.home,name="home"),
+    path("add/",views.add,name="add"),
+    path("edit/<int:id>/",views.edit,name="edit"),
   
 ]
